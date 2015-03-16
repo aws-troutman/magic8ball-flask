0181fba45459a14ca234494e55ade5cf16b0270c
Ubuntu <ubuntu@ip-172-31-0-37.us-west-2.compute.internal>
2015-03-16 22:40:05 +0000

    update init

--- magic8ball/magic8ball/__init__.py
+++ magic8ball/magic8ball/__init__.py
@@ -3,38 +3,36 @@ from flask_bootstrap import Bootstrap
 import random
 import boto.utils
 
-def create_app():
-  app = Flask(__name__)
-  Bootstrap(app)
+app = Flask(__name__)
+Bootstrap(app)
 
-  @app.route('/magic')
-  def index():
-    responses = [
-      'It is certain',
-      'It is decidedly so',
-      'Without a doubt',
-      'Yes definitely',
-      'You may rely on it',
-      'As I see it, yes',
-      'Most likely',
-      'Outlook good',
-      'Yes',
-      'Signs point to yes',
-      'Reply hazy try again',
-      'Ask again later',
-      'Better not tell you now',
-      'Cannot predict now',
-      'Concentrate and ask again',
-      'Don\'t count on it',
-      'My reply is no',
-      'My sources say no',
-      'Outlook not so good',
-      'Very doubtful'];
-    instance_id = boto.utils.get_instance_metadata()['instance-id']
-    random_choice = random.choice(responses)
-    
-    return render_template('magic.html', instance_id=instance_id, random_choice=random_choice)
-  return app
+@app.route('/magic')
+def index():
+  responses = [
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'Yes definitely',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most likely',
+    'Outlook good',
+    'Yes',
+    'Signs point to yes',
+    'Reply hazy try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don\'t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful'];
+  instance_id = boto.utils.get_instance_metadata()['instance-id']
+  random_choice = random.choice(responses)
+  
+  return render_template('magic.html', instance_id=instance_id, random_choice=random_choice)
 
 if __name__ == '__main__':
-    create_app().run() #'0.0.0.0', 5000 debug=True)
+    app.run() #'0.0.0.0', 5000 debug=True)
