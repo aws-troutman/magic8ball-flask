202eb3b1c6978d4086cc0a53dc463deae0c3b320
Ubuntu <ubuntu@ip-172-31-0-37.us-west-2.compute.internal>
2015-03-17 04:23:10 +0000

    syntax error

--- magic8ball/magic8ball/__init__.py
+++ magic8ball/magic8ball/__init__.py
@@ -29,21 +29,21 @@ def get_fortunte():
     'Outlook not so good',
     'Very doubtful'];
 
-    return random.choice(responses)
+  return random.choice(responses)
 
 @app.route('/_get_new_fortune')
 def get_new_fortune():
-    random_choice = get_fortune()
-    instance_id = boto.utils.get_instance_metadata()['instance-id']
+  random_choice = get_fortune()
+  instance_id = boto.utils.get_instance_metadata()['instance-id']
     
-    return jsonify([random_choice, instance_id])
+  return jsonify([random_choice, instance_id])
 
 @app.route('/magic')
 def index():
-    instance_id = boto.utils.get_instance_metadata()['instance-id']
-    random_choice = get_fortune()
+  instance_id = boto.utils.get_instance_metadata()['instance-id']
+  random_choice = get_fortune()
 
-    return render_template('magic.html', instance_id=instance_id, random_choice=random_choice)
+  return render_template('magic.html', instance_id=instance_id, random_choice=random_choice)
 
 if __name__ == '__main__':
     app.run() #'0.0.0.0', 5000 debug=True)
